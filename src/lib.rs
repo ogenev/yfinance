d232a3ba50b714c83e86b5ec7aa1aa8ab921688f
Ognyan Genev <ognian.genev@gmail.com>
2020-04-20 16:38:20 +0300

    remove tokio spawn

--- src/lib.rs
+++ src/lib.rs
@@ -12,33 +12,28 @@ impl<'a> Downloader<'a> {
             ticker_list
         }
     }
+
     #[tokio::main]
-    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut tasks: Vec<tokio::task::JoinHandle<Result<(), ()>>> = vec![];
+    pub async fn run(&self) {
+        let mut tasks = vec![];
         for ticker in &self.ticker_list {
-            let url = self.base_url.to_owned() + ticker;
-            tasks.push(tokio::spawn(async move {
-                match reqwest::get(&url).await {
-                    Ok(resp) => {
-                        match resp.text().await {
-                            Ok(text) => {
-                                println!("RESPONSE: {} bytes from {}", text.len(), url);
-                            }
-                            Err(_) => println!("ERROR reading {}", url),
-                        }
-                    }
-                    Err(_) => println!("ERROR downloading {}", url),
-                }
-                Ok(())
-            }));
+            tasks.push(self.query_ticker(&ticker))
         }
-        println!("Started {} tasks. Waiting...", tasks.len());
-        join_all(tasks).await;
-        Ok(())
+        // wait and return all futures
+        let results = join_all(tasks).await;
+        println!("{:?}", results);
+    }
+
+    async fn query_ticker(&self, ticker: &str) -> Result<String, reqwest::Error>{
+        let url = self.base_url.to_owned() + ticker;
+        let response  = reqwest::get(&url).await.unwrap();
+        //response.json::<Body>().await
+        response.text().await
+
     }
 }
 
 pub fn test() {
-    let downloader = Downloader::new(vec!["TSLA", "MSFT", "A"]);
-    downloader.run().unwrap_or_else(|error| println!("{}", error));
+    let downloader = Downloader::new(vec!["TSLA", "MSFT", "A", ]);
+    downloader.run();
 }
